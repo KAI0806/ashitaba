5e798c6a44d605d9e40e2ed6645fca8f86d5487b
MASATAKE KAI <nerotan0806@gmail.com>
2021-08-24 17:53:38 +0900

    バグ修正

--- main.js
+++ main.js
@@ -7,3 +7,36 @@ function hamburger(){
   document.getElementById('hamburger').addEventListener('click',function(){
     hamburger();
   });
+  document.getElementById('nav').addEventListener('click',function(){
+    hamburger();
+  });
+
+function() {
+  const target     = document.getElementById('video-area'),
+        height     = 56;
+  let offset       = 0,
+      lastPosition = 0,
+      ticking      = false;
+
+  function onScroll() {
+    if (lastPosition > height) {
+      if (lastPosition > offset) {
+        target.classList.add('head-animation');
+      } else {
+        target.classList.remove('head-animation');
+      }
+      offset = lastPosition;
+    }
+  }
+  
+  window.addEventListener('scroll', function(e) {
+    lastPosition = window.scrollY;
+    if (!ticking) {
+      window.requestAnimationFrame(function() {
+        onScroll(lastPosition);
+        ticking = false;
+      });
+      ticking = true;
+    }
+  });
+});
